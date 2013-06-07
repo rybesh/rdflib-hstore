0b6234f775a35b78e0e97231e1b7587d261aecd5
Ryan Shaw <ryanshaw@unc.edu>
2013-06-06 21:15:00 -0400

    Restructuring tests.

--- rdflib_hstore/tests/core.py
+++ rdflib_hstore/tests/core.py
@@ -1,10 +1,12 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 import os
 import rdflib
 import psycopg2
 from psycopg2.extensions import \
     ISOLATION_LEVEL_AUTOCOMMIT, ISOLATION_LEVEL_READ_COMMITTED
-from rdflib import plugin
+from rdflib import plugin, URIRef, Literal
 from rdflib.store import Store
 from rdflib.graph import Graph, ConjunctiveGraph
 
@@ -13,6 +15,19 @@ storetest = True
 connection_uri = os.environ.get(
     'DBURI',
     'postgresql://unittest@localhost/rdflibhstore_test')
+
+michel = URIRef(u'michel')
+tarek = URIRef(u'tarek')
+bob = URIRef(u'bob')
+likes = URIRef(u'likes')
+hates = URIRef(u'hates')
+pizza = URIRef(u'pizza')
+cheese = URIRef(u'cheese')
+says = URIRef(u'says')
+hello = Literal(u'hello', lang='en')
+konichiwa = Literal(u'こんにちは', lang='ja')
+something = Literal(u'something')        
+
 
 class BaseCase(unittest.TestCase):
 
@@ -39,13 +54,37 @@ class BaseCase(unittest.TestCase):
             g.close()
         self.execute('DROP')
 
-class TestHstoreGraphCore(BaseCase):
+class TestHstoreGraph(BaseCase):
 
     def open_graph(self):
         graph = Graph(store='hstore')
         graph.open(connection_uri, create=True)
         self.graphs.append(graph)
         return graph
+
+    def add_stuff(self, graph):
+        graph.add((tarek, likes, pizza))
+        graph.add((tarek, likes, cheese))
+        graph.add((michel, likes, pizza))
+        graph.add((michel, likes, cheese))
+        graph.add((bob, likes, cheese))
+        graph.add((bob, hates, pizza))
+        graph.add((bob, hates, michel))
+        graph.add((bob, says, hello))
+        graph.add((bob, says, konichiwa))
+        graph.add((bob, says, something))
+
+    def remove_stuff(self, graph):
+        graph.remove((tarek, likes, pizza))
+        graph.remove((tarek, likes, cheese))
+        graph.remove((michel, likes, pizza))
+        graph.remove((michel, likes, cheese))
+        graph.remove((bob, likes, cheese))
+        graph.remove((bob, hates, pizza))
+        graph.remove((bob, hates, michel))
+        graph.remove((bob, says, hello))
+        graph.remove((bob, says, konichiwa))
+        graph.remove((bob, says, something))        
 
     def test_namespaces(self):
         graph = self.open_graph()
@@ -63,14 +102,58 @@ class TestHstoreGraphCore(BaseCase):
 
     def test_add_triples(self):
         graph = self.open_graph()
-        michel = rdflib.URIRef(u'michel')
-        likes = rdflib.URIRef(u'likes')
-        pizza = rdflib.URIRef(u'pizza')
-        cheese = rdflib.URIRef(u'cheese')
-        graph.add((michel, likes, pizza))
-        graph.add((michel, likes, cheese))
-        ntriples = graph.triples((None, None, None))
-        self.assertTrue(len(list(ntriples)) == 2)
+        self.add_stuff(graph)
+        self.assertTrue(len(list(graph.triples((None, None, None)))) == 10)
+
+    def test_remove_triples(self):
+        graph = self.open_graph()
+        self.add_stuff(graph)
+        self.assertTrue(len(list(graph.triples((None, None, None)))) == 10)
+        self.remove_stuff(graph)
+        self.assertTrue(len(list(graph.triples((None, None, None)))) == 0)
+
+    def test_triples(self):
+        graph = self.open_graph()
+        self.add_stuff(graph)
+        asserte = self.assertEquals
+
+        # unbound subjects
+        asserte(len(list(graph.triples((None, likes, pizza)))), 2)
+        asserte(len(list(graph.triples((None, hates, pizza)))), 1)
+        asserte(len(list(graph.triples((None, likes, cheese)))), 3)
+        asserte(len(list(graph.triples((None, hates, cheese)))), 0)
+
+        # unbound objects
+        asserte(len(list(graph.triples((michel, likes, None)))), 2)
+        asserte(len(list(graph.triples((tarek, likes, None)))), 2)
+        asserte(len(list(graph.triples((bob, hates, None)))), 2)
+        asserte(len(list(graph.triples((bob, likes, None)))), 1)
+
+        # unbound predicates
+        asserte(len(list(graph.triples((michel, None, cheese)))), 1)
+        asserte(len(list(graph.triples((tarek, None, cheese)))), 1)
+        asserte(len(list(graph.triples((bob, None, pizza)))), 1)
+        asserte(len(list(graph.triples((bob, None, michel)))), 1)
+
+        # unbound subject, objects
+        asserte(len(list(graph.triples((None, hates, None)))), 2)
+        asserte(len(list(graph.triples((None, likes, None)))), 5)
+
+        # unbound predicates, objects
+        asserte(len(list(graph.triples((michel, None, None)))), 2)
+        asserte(len(list(graph.triples((bob, None, None)))), 6)
+        asserte(len(list(graph.triples((tarek, None, None)))), 2)
+
+        # unbound subjects, predicates
+        asserte(len(list(graph.triples((None, None, pizza)))), 3)
+        asserte(len(list(graph.triples((None, None, cheese)))), 3)
+        asserte(len(list(graph.triples((None, None, michel)))), 1)
+        asserte(len(list(graph.triples((None, None, konichiwa)))), 1)
+
+        # all unbound
+        asserte(len(list(graph.triples((None, None, None)))), 10)
+        self.remove_stuff(graph)
+        asserte(len(list(graph.triples((None, None, None)))), 0)
 
     def test_reopening_db(self):
         graph = self.open_graph()
@@ -91,7 +174,7 @@ class TestHstoreGraphCore(BaseCase):
                 'postgresql://unittest@localhost/nosuchdb',
                 create=False)
 
-class TestHstoreConjunctiveGraphCore(BaseCase):
+class TestHstoreConjunctiveGraph(BaseCase):
 
     def open_graph(self):
         graph = ConjunctiveGraph(store='hstore')
